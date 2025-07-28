15a85697362f2908cd65674ad782894413f517f5
Phyo Htet Paing <phyohtetpaing200303@gmail.com>
2025-07-28 15:01:12 +0700

    Resume Feedback Page

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -3,7 +3,7 @@ import { useLocation, useNavigate } from "react-router";
 import { usePuterStore } from "~/lib/puter";
 
 export const meta = () => [
-  { title: "Resuming | Auth" },
+  { title: "Resumind | Auth" },
   { name: "descroption", content: "Log into your account" },
 ];
 
